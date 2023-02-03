3703e2dbd8695827ca76998c077f565764537beb
YUNAKIM <devops.yuna@gmail.com>
2023-02-03 09:03:41 +0900

    2023-02-03 백준 2292 벌집
    2023-02-03 백준 2292 벌집

--- honeycomb/src/main/java/HoneyComb.java
+++ honeycomb/src/main/java/HoneyComb.java
@@ -5,15 +5,13 @@ public class HoneyComb {
     public static void main(String[] agrs) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        int count = 0;
+        int count = 1;
         int startNum = 1;
 
-        while (true) {
-            (startNum + (6 * count) <= number)? count++:break;
+        while (number > startNum) {
+            startNum += 6*count; // 6의 배수만큼 벌집이 증가
+            count++; // 지나쳐온 벌집껍질의 개수
         }
-            if (startNum + (6 * count) <= number) {
-                count++;
-            else
-        }
+        System.out.println(count);
     }
 }
