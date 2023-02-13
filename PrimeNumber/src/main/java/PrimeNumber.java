bf327c22379f15f78046ed01ccb590de0fbb33f0
YUNAKIM <devops.yuna@gmail.com>
2023-02-14 00:28:04 +0900

    2023-02-10 백준 1978 소수 찾기
    2023-02-10 백준 1978 소수 찾기

--- PrimeNumber/src/main/java/PrimeNumber.java
+++ PrimeNumber/src/main/java/PrimeNumber.java
@@ -3,8 +3,28 @@ public class PrimeNumber {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int M = sc.nextInt();
-        int N = sc.nextInt();
+        int count = 0;
+        boolean prime = false;
 
+        for (int i=0; i<M; i++){
+            int num = sc.nextInt();
+            prime =isPrime(num);
+            if (prime == true){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 
+    public static boolean isPrime(int number){
+        if (number ==1) {
+            return false;
+        }
+        for(int i=2; i<number; i++) {
+            if(number%i==0){
+                return false;
+            }
+        }
+        return true;
     }
 }
